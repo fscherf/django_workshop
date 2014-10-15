fa697c8078e72e7b3dbefd2e41d082f7c81400f5
Florian Scherf <fscherf@gmx.net>
2014-10-15 16:20:31 +0200

    blog: add simple BlogPost data model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    author = models.ForeignKey(
+        User,
+        related_name='+',
+        verbose_name='Author',
+        blank=True,
+        null=True
+    )
+
+    published = models.BooleanField(
+        verbose_name='Published',
+        default=False
+    )
+
+    title = models.CharField(
+        verbose_name='Title',
+        blank=False,
+        max_length=35
+    )
+
+    content = models.TextField(
+        verbose_name='Content',
+    )
+
+    created = models.DateTimeField(
+        verbose_name='Created',
+        auto_now_add=True
+    )
